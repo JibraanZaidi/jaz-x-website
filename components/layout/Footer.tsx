937d05bff0ebe6e04d184ea76e7c30b3f9232876
JibraanZaidi <jibraanali@gmail.com>
2026-07-02 23:58:54 +0500

    Updated website

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -45,7 +45,7 @@ export default function Footer() {
 
           <div className="flex items-center gap-4 text-white/40">
             <a
-              href="#"
+              href="https://x.com/JAZXinnovation"
               aria-label="Twitter"
               className="hover:text-secondary transition-colors"
             >
@@ -61,7 +61,7 @@ export default function Footer() {
             </a>
 
             <a
-              href="#"
+              href="https://www.linkedin.com/in/jaz-x-37561a41b/"
               aria-label="LinkedIn"
               className="hover:text-secondary transition-colors"
             >
